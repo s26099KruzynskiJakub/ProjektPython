2793aa96ab952624d782dd71f2a8e8a3eab948da
DontPanic1303 <118015465+DontPanic1303@users.noreply.github.com>
2023-06-14 14:27:58 +0200

    com 2

---
+++ Project/gui.py
@@ -0,0 +1,12 @@
+class GUI:
+    def __init__(self):
+        #konstuktor
+        self.cos = 5
+
+    #metoda init towrzy podstawowe gui wraz z menu wyłowuje metodę search bez nazwy i order by do stworzenia listy produktów
+    #metoda search słurzy do sortownia można szukać po nazwa i id oraz order by może uwzględniać produktów kturych nie ma na magazynie
+    #metoda createRaport tworzy raport
+    #metoda modyfikujProdukt zmienia informacje o produkcie
+    #metoda usunProdukt usuwa produkt
+    #metoda dodajProdukt z wyświetlProdukty może updejtuje stary jak dodajemy taki sam
+  
