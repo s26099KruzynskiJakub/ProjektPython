dbfd1da03af298ec1a3cefd547144a7de57d6d00
Jacobek <kuba.kruzynski@o2.pl>
2023-06-14 16:14:56 +0200

    wurk

--- Project/gui.py
+++ Project/gui.py
@@ -2,14 +2,38 @@ import tkinter as tk
 
 class GUI:
     def __init__(self):
-        self.main_window = tk.Tk()
 
-        # Top Frame
-        self.top_frame = tk.Frame(self.main_window)
+        self.main_window = tk.Tk()
         self.main_window.title("MAGAZYN")
+
+        self.top_frame = tk.Frame(self.main_window)
         self.top_frame.pack(side="top")
-        self.top_label = tk.Label(self.top_frame, text="Top Frame")
-        self.top_label.pack()
+
+        # Button to select value (Name or ID)
+        self.text = tk.Text(self.top_frame,insertbackground='blue')
+        self.text.pack(side='left',pady=10)
+        self.text.config(width=10, height=1)
+
+        # Separator Line
+        self.separator_lineFirst = tk.Frame(self.top_frame, width=2, bd=1, relief=tk.SUNKEN)
+        self.separator_lineFirst.pack(side='left', padx=5, pady=5, fill='y')
+
+        # Checkbuttons for advanced search attributes
+        self.attribute_var = tk.StringVar()
+        self.attribute_checkbuttons = []
+        attributes = ["ID", "Nazwa", "Opis", "Ocena", "Ilość", "Miara"]
+        for attribute in attributes:
+            checkbutton = tk.Checkbutton(self.top_frame, text=attribute, variable=self.attribute_var, onvalue=attribute,
+                                         offvalue="")
+            checkbutton.pack(anchor="w",side='left')
+            self.attribute_checkbuttons.append(checkbutton)
+
+         # Separator Line
+        self.separator_line = tk.Frame(self.top_frame, width=2, bd=1, relief=tk.SUNKEN)
+        self.separator_line.pack(side='left', padx=5, pady=5, fill='y')
+        self.magazine_var = tk.BooleanVar()
+        self.magazine_button = tk.Checkbutton(self.top_frame, text="Must be in magazine", variable=self.magazine_var)
+        self.magazine_button.pack(anchor="w",fill="both")  # Align to the left
 
         # Separator Line
         self.separator1 = tk.Frame(self.main_window, height=5, bd=1, relief=tk.SUNKEN)
@@ -34,7 +58,25 @@ class GUI:
         # Adjust weight to make the middle frame bigger
         self.main_window.grid_rowconfigure(2, weight=1)
 
+
+
+        self.text.bind("<Return>", self.call_method)
         tk.mainloop()
+
+    def call_method(self,event=None):
+        text_input = self.text.get("1.0", "end-1c")  # Retrieve text from the text field
+        print(text_input)  # Call the method with the text input
+        self.text.delete("1.0", "end")
+
+
+
+    def select_value(self):
+        # Function to handle the button click event
+        selected_value = self.value_button.cget("text")
+        if selected_value == "Name":
+            self.value_button.configure(text="ID")
+        else:
+            self.value_button.configure(text="Name")
 
 # Create an instance of the GUI class
 gui = GUI()
